274f46423b22da1f923bc22fb6d82690c190bb85
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-02-03 00:04:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ from bs4 import BeautifulSoup
 import os
 import re
 
-def savewithtext(need_step,step_name,problem_num,problem_sub,problem_des,make_step_dir,file_namelist):
-    problem_sublist=[]
-    savetext=open(f'output{need_step}.txt','w')
+
+def savewithtext(
+    need_step,
+    step_name,
+    problem_num,
+    problem_sub,
+    problem_des,
+    make_step_dir,
+    file_namelist,
+):
+    problem_sublist = []
+    savetext = open(f"output{need_step}.txt", "w")
     # 블로그 html 출력부
-    savetext.write(f'[백준/python] {step_name} 전체 풀이({need_step}단계)')
-    savetext.write('''
+    savetext.write(f"[백준/python] {step_name} 전체 풀이({need_step}단계)")
+    savetext.write(
+        """
     <p data-ke-size="size16">&nbsp;</p>
     <!-- 목차 부분 -->
     <div class="book-toc">
@@ -19,26 +29,34 @@ def savewithtext(need_step,step_name,problem_num,problem_sub,problem_des,make_st
     <p data-ke-size="size16">&nbsp;</p>
     <p data-ke-size="size16"><br /><br /></p>
     <!-- 제목 -->
-    ''')
+    """
+    )
     for i in range(len(problem_num)):
-        file_name=file_namelist[i]
+        file_name = file_namelist[i]
         make_problem_dir = f"{make_step_dir}/{file_name}"
         if file_name in os.listdir(make_step_dir):
-            print("동일 이름을 가진 폴더가 존재합니다." , file_name)
+            print("동일 이름을 가진 폴더가 존재합니다.", file_name)
         else:
             os.mkdir(str(make_problem_dir))
-        pytext = open(f'{make_problem_dir}/{file_name}.py','r').read()
-        readme = open(f'{make_problem_dir}/README.md','r').read()
+        pytext = open(f"{make_problem_dir}/{file_name}.py", "r").read()
+        readme = open(f"{make_problem_dir}/README.md", "r").read()
         problem_sublist.append(problem_sub[i])
-        savetext.write(f'<h3 data-ke-size="size23"><b><span style="font-family: \'Noto Serif KR\';">{problem_sub[i]}<b>(#{problem_num[i]})</b></b></h3>')
-        savetext.write('''
+        savetext.write(
+            f'<h3 data-ke-size="size23"><b><span style="font-family: \'Noto Serif KR\';">{problem_sub[i]}<b>(#{problem_num[i]})</b></b></h3>'
+        )
+        savetext.write(
+            """
         <!-- Hint 란 -->
         <ul style="list-style-type: disc;" data-ke-list-type="disc">
         <li><b><span style="font-family: 'Noto Serif KR';">Problem</span></b></li>
         </ul>
-        ''')
-        savetext.write(f'<p data-ke-size="size16"><span style="font-family: \'Noto Serif KR\';">{problem_des[i]}</p>')
-        savetext.write('''
+        """
+        )
+        savetext.write(
+            f'<p data-ke-size="size16"><span style="font-family: \'Noto Serif KR\';">{problem_des[i]}</p>'
+        )
+        savetext.write(
+            """
         <p data-ke-size="size16"><span style="font-family: 'Noto Serif KR';">&nbsp;</p>
         <ul style="list-style-type: disc;" data-ke-list-type="disc">
         
@@ -57,18 +75,22 @@ def savewithtext(need_step,step_name,problem_num,problem_sub,problem_des,make_st
         <!-- 코드 삽입란 -->
         <pre class="vim">
             <code>
-        ''')
-        savetext.write(f'\n{pytext}')
-        savetext.write('''
+        """
+        )
+        savetext.write(f"\n{pytext}")
+        savetext.write(
+            """
             </code>
         </pre>
         <p data-ke-size="size16"><span style="font-family: 'Noto Serif KR';">&nbsp;</p>
         
         <p data-ke-size="size16"><span style="font-family: 'Noto Serif KR';">
         
-        ''')
-        savetext.write(f'\n{readme}')
-        savetext.write('''
+        """
+        )
+        savetext.write(f"\n{readme}")
+        savetext.write(
+            """
         </p>
         
         <!-- 구분선 -->
@@ -76,7 +98,8 @@ def savewithtext(need_step,step_name,problem_num,problem_sub,problem_des,make_st
         <p data-ke-size="size16"><span style="font-family: 'Noto Serif KR';">&nbsp;</p>
         <p data-ke-size="size16"><span style="font-family: 'Noto Serif KR';">contents 02</p>
         <p data-ke-size="size16"><span style="font-family: 'Noto Serif KR';">&nbsp;</p>
-        ''')
+        """
+        )
 
     # 태그 입력을 위한 키워드
     print(*problem_sublist)
@@ -93,77 +116,78 @@ def savewithtext(need_step,step_name,problem_num,problem_sub,problem_des,make_st
 
     savetext.close()
 
+
 def urlrequest(url):
     """url header 변형으로 크롤링"""
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
     return response
 
+
 def mkdir(root_path, step_name):
     make_step_dir = f"{root_path}/{step_name}"
     if step_name in os.listdir(root_path):
-        print("동일 이름을 가진 폴더가 존재합니다." , step_name)
+        print("동일 이름을 가진 폴더가 존재합니다.", step_name)
     else:
         os.mkdir(make_step_dir)
     return make_step_dir
 
-def mkproblemdir(make_step_dir,file_name):
-    file_name = re.sub(r"[\/*?<>|]","MoD",file_name)
+
+def mkproblemdir(make_step_dir, file_name):
+    file_name = re.sub(r"[\/*?<>|]", "MoD", file_name)
     make_problem_dir = f"{make_step_dir}/{file_name}"
     if file_name in os.listdir(make_step_dir):
-        print("동일 이름을 가진 폴더가 존재합니다." , file_name)
+        print("동일 이름을 가진 폴더가 존재합니다.", file_name)
     else:
         os.mkdir(str(make_problem_dir))
-    open(f'{make_problem_dir}/{file_name}.py','w')
-    open(f'{make_problem_dir}/README.md','w')
+    open(f"{make_problem_dir}/{file_name}.py", "w")
+    open(f"{make_problem_dir}/README.md", "w")
 
-root_path = "/root/Github/BOJ-python"
-main_url = 'https://www.acmicpc.net'
-url = f'{main_url}/step'
 
+root_path = "/home/chos/Github/BOJ-python"
+main_url = "https://www.acmicpc.net"
+url = f"{main_url}/step"
 
 
 # Step 가져오기
 step_list = []
 # 문제 번호
-problem_num=[]
+problem_num = []
 # 문제 소제목
-problem_sub=[]
+problem_sub = []
 # 문제 설명
-problem_des=[]
+problem_des = []
 
 whole_response = urlrequest(url)
 # 응답 코드가 200일시 *정상적으로 가져왔을시에
 if whole_response.status_code == 200:
     html = whole_response.text
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, "html.parser")
     main = soup.find("div", {"class": "table-responsive"})
     body = main.find("tbody")
     links = body.find_all("tr")
     for link in links:
-        lnk = link.find("a").attrs['href']
-        steps = main_url+lnk
+        lnk = link.find("a").attrs["href"]
+        steps = main_url + lnk
         step_list.append(steps)
 
-# 필요한 링크를 모두 가져왔다.
+    # 필요한 링크를 모두 가져왔다.
     need_step = int(input("필요한 단계를 입력하시오."))
-    steps = step_list[int(need_step)-1]
+    steps = step_list[int(need_step) - 1]
 
-# request를 해오고 이를 BS로 html을 파싱해온다.
+    # request를 해오고 이를 BS로 html을 파싱해온다.
     step_response = urlrequest(steps)
     if step_response.status_code == 200:
         step_html = step_response.text
-        step_soup = BeautifulSoup(step_html, 'html.parser')
-        step_main = step_soup.find(
-            "div", {"class": "table-responsive"})
-        step_name = step_soup.find(
-            "div", {"class": "breadcrumbs"}).text
+        step_soup = BeautifulSoup(step_html, "html.parser")
+        step_main = step_soup.find("div", {"class": "table-responsive"})
+        step_name = step_soup.find("div", {"class": "breadcrumbs"}).text
         step_body = step_main.find("tbody")
         pbs = step_body.find_all("tr")
         cnt = 1
         # Step 에 해당하는 폴더 생성하기
         make_step_dir = mkdir(root_path, step_name)
 
-        file_namelist=[]
+        file_namelist = []
 
         setmode = int(input("블로그 포스팅용 html을 원한다면 1 을 새로운 단계를 가져오려면 0 을 입력하세요"))
         for pb in pbs[0::2]:
@@ -172,36 +196,44 @@ if whole_response.status_code == 200:
             file_name = f"{str(cnt).zfill(2)}#{nums}_{subs}"
             # 문제별 폴더, 파일(.py, README) 생성하기
             file_namelist.append(file_name)
-            if setmode ==0:
-                mkproblemdir(make_step_dir,file_name)
-            cnt+=1
+            if setmode == 0:
+                mkproblemdir(make_step_dir, file_name)
+            cnt += 1
             problem_num.append(nums)
             problem_sub.append(subs)
-            pblink = pb.find("a").attrs['href']
-            pbcontent = main_url+pblink
-            pbcontent_response = requests.get(pbcontent, headers={"User-Agent": "Mozilla/5.0"})
-            
+            pblink = pb.find("a").attrs["href"]
+            pbcontent = main_url + pblink
+            pbcontent_response = requests.get(
+                pbcontent, headers={"User-Agent": "Mozilla/5.0"}
+            )
+
             if pbcontent_response.status_code == 200:
                 pbcontent_html = pbcontent_response.text
-                pbcontent_soup = BeautifulSoup(pbcontent_html, 'html.parser')
+                pbcontent_soup = BeautifulSoup(pbcontent_html, "html.parser")
                 pbcontent_main = pbcontent_soup.find("div", {"id": "problem-body"})
-                pcs = pbcontent_main.find("div",{"class": "problem-text"}).text
+                pcs = pbcontent_main.find("div", {"class": "problem-text"}).text
                 problem_des.append(pcs)
-                
+
             else:
-                print(f'There is no content in {nums}')             
+                print(f"There is no content in {nums}")
     else:
-        print(f'errorcode: {step_response.status_code} at {steps}step')
+        print(f"errorcode: {step_response.status_code} at {steps}step")
     if len(problem_num) == len(problem_sub):
-        print('work correctly!')
+        print("work correctly!")
     else:
-        print('something is wrong..')      
+        print("something is wrong..")
 else:
-    print("파싱 에러",whole_response.status_code)
+    print("파싱 에러", whole_response.status_code)
     raise ConnectionError("크롤링 HTML 가져오지 못하였음")
 
 
-
 if setmode == 1:
-    savewithtext(need_step,step_name,problem_num,problem_sub,problem_des,make_step_dir,file_namelist)
-
+    savewithtext(
+        need_step,
+        step_name,
+        problem_num,
+        problem_sub,
+        problem_des,
+        make_step_dir,
+        file_namelist,
+    )
